ec4d58a818c2e253945299173d8998731ebb8901
LuigiSalvatore <134612508+LuigiSalvatore@users.noreply.github.com>
2023-08-16 10:31:13 -0300

    codigo de soma

--- soma.asm
+++ soma.asm
@@ -1,1 +1,20 @@
+.text
+.global main
+
+main:
+    li      $v0,    5               # loads instantly the value '5' into $v0 (getchar)
+    syscall 
+    move    $t0,    $v0             # moves value in $v0 to $t0
+
+    li      $v0,    5               # loads instantly the value '5' into $v0 (getchar)
+    syscall 
+    move    $t1,    $v0             # moves value in $v0 to $t1
+
+    add     $t2,    $t1,    $t0     # adds $t1 and $t0 Ans goes to $t2
+    move    $a0,    $t2             # moves $t2 to $a0 (buffer for print)
+    li      $v0,    1               # loads instantly the value '1' into $v0 (printf)
+    syscall 
+
+    li      $v0,    10              # exit code
+    syscall 
 
